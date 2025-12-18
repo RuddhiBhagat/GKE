13ea3ab67a15681fb1d5c2684ac478993a02f59a
RuddhiBhagat <ruddhi.bhagat@atyeti.com>
2025-12-18 13:01:12 +0530

    adding gke files

---
+++ lab-1/modules/gke/variables.tf
@@ -0,0 +1,35 @@
+variable "project_id" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+
+variable "cluster_name" {
+  type = string
+}
+
+variable "network_id" {
+  type = string
+}
+
+variable "subnet_id" {
+  type = string
+}
+
+variable "cluster_secondary_range_name" {
+  type = string
+  description = "Name of the secondary IP range for Pods (from VPC module)"
+}
+
+variable "services_secondary_range_name" {
+  type = string
+  description = "Name of the secondary IP range for Services (from VPC module)"
+}
+variable "enable_cluster" {
+  type        = bool
+  description = "Enable or disable GKE cluster"
+}
+
+
